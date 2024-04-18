cc3c02f0a6eeac07fc2ec67cc56a8e88f99667d6
Jashan <156020750+jashanpreetjabbal@users.noreply.github.com>
2024-04-18 10:10:15 -0700

    Update script.js

--- Face Expression Detection/script.js
+++ Face Expression Detection/script.js
@@ -1,18 +1,18 @@
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
-const videoContainer = document.querySelector('.video-container');
+const startRecordingBtn = document.getElementById('startRecording');
+const endRecordingBtn = document.getElementById('endRecording');
+
+let isRecording = false;
+let recordedEmotions = [];
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('models'),
+  faceapi.nets.tinyFaceDetector.loadFromUri('models/'),
   faceapi.nets.faceLandmark68Net.loadFromUri('models/'),
   faceapi.nets.faceRecognitionNet.loadFromUri('models/'),
   faceapi.nets.faceExpressionNet.loadFromUri('models/')
 ]).then(startVideo)
 .catch(err => console.error(err)); 
-
-function flipVideo() {
-  video.classList.toggle('flip-horizontal');
-}
 
 async function startVideo() {
   try {
@@ -27,15 +27,40 @@ async function startVideo() {
   }
 }
 
+function startRecording() {
+  isRecording = true;
+  canvas.style.display = 'block';
+}
+
+function endRecording() {
+  isRecording = false;
+  console.log(recordedEmotions);
+  recordedEmotions = []; 
+  canvas.style.display = 'none';
+}
+
 video.addEventListener('play', () => {
   const displaySize = { width: video.width, height: video.height };
   faceapi.matchDimensions(canvas, displaySize);
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-    faceapi.draw.drawDetections(canvas, resizedDetections);
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    if (isRecording) {
+      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+      faceapi.draw.drawDetections(canvas, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+
+      
+      resizedDetections.forEach(detection => {
+        const expressions = detection.expressions;
+        const dominantEmotion = Object.keys(expressions).reduce((a, b) => expressions[a] > expressions[b] ? a : b);
+        recordedEmotions.push({ time: new Date(), emotion: dominantEmotion });
+      });
+    }
   }, 100);
 });
+
+startRecordingBtn.addEventListener('click', startRecording);
+endRecordingBtn.addEventListener('click', endRecording);
+
